289ef05d7c0656cebdc1b70a0be022fe92ab2efa
santiago-federici <santiagofederici88@gmail.com>
2024-03-27 08:03:48 -0300

    created the connection to MongoDB

---
+++ src/utils/DBconnection.ts
@@ -0,0 +1,17 @@
+import mongoose, { ConnectOptions } from 'mongoose'
+
+const connectionString = process.env.MONGODB_URI || ''
+
+export const connectDB = async () => {
+  try{
+    await mongoose.connect(connectionString, {
+      dbName: 'nextjs-ecommerce',
+      useNewUrlParser: true,
+    } as ConnectOptions)
+    console.log('Database connected')
+  } catch (err) {
+    console.error(err)
+  }
+}
+    
+    
