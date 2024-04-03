4cc868cce40abebbcb7c019d367bbd90507c42cc
santiago-federici <santiagofederici88@gmail.com>
2024-04-03 11:20:33 -0300

    created Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>HomePage</main>
+    <main className="wrapper grid">
+      <section className="hero-section flex flex-col items-center md:items-start gap-12 bg-red-500">
+        {/* <Image
+          src={'/public/heroGlasses.png'}
+          width={150} 
+          height={150}
+          alt="Hero image of glasses with green background"
+        /> */}
+        <h1 className="text-white text-center md:text-left text-pretty">Mejora tu estilo con nuestro productos</h1>
+
+        <button className="ghost-btn">Ver productos</button>
+      </section>
+    </main>
   );
 }
