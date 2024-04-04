49661273acad5a0a0dca57119435671dc28b0481
santiago-federici <santiagofederici88@gmail.com>
2024-04-04 09:06:23 -0300

    adding a height to the HeroSection, in order to separate it from other content

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,24 +3,24 @@ import Image from "next/image";
 
 export function HeroSection () {
   return (
-    <>
-    <div className="hero">
-      <Image
-        src="/heroGlasses.jpg"
-        alt="Hero Image"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-        priority
-        />
-    </div>
+    <div className="h-[85dvh]">
+      <div className="hero">
+        <Image
+          src="/heroGlasses.jpg"
+          alt="Hero Image"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+          />
+      </div>
 
-    <section className="wrapper mt-14 md:mt-40 grid justify-items-center md:justify-items-start w-full">
-      <h2 className="text-white font-bold text-5xl md:text-6xl text-balance text-center lg:text-left lg:w-[500px]">
-        Mejora tu estilo con nuestros productos
-      </h2>
-      <Button as="ghost" text="Ver productos" extraClassName="mt-10" />
-    </section>
-    </>
+      <section className="wrapper mt-14 md:mt-40 grid justify-items-center md:justify-items-start w-full">
+        <h2 className="text-white font-bold text-5xl md:text-6xl text-balance text-center lg:text-left lg:w-[500px]">
+          Mejora tu estilo con nuestros productos
+        </h2>
+        <Button as="ghost" text="Ver productos" extraClassName="mt-10" />
+      </section>
+    </div>
   )
 }
