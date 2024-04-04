0b7a9e3309dc0ab04dcfe0dbe7f4919a7f8d5958
santiago-federici <santiagofederici88@gmail.com>
2024-04-04 19:15:28 -0300

    set primary color to black

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
 export function Button ({ as, text, extraClassName }: { as: string, text: string, extraClassName?: string }) {
 
-  const defeaultButtonStyle = 'p-[--btn-padding] rounded text-white font-bold'
+  const defeaultButtonStyle = 'p-btn-padding rounded text-white font-bold'
   
-  const primaryButtonStyle = 'bg-black font-medium'
-  const ghostButtonStyle = 'bg-transparent border border-[2px] border-[--clr-secondary] font-bold'
+  const primaryButtonStyle = 'bg-primary font-medium'
+  const ghostButtonStyle = 'bg-transparent border-secondary font-bold border border-[2px]'
   
   return (
     <button className={`${defeaultButtonStyle} ${as === 'primary' ? primaryButtonStyle : ghostButtonStyle} ${extraClassName}`}>{text}</button>
