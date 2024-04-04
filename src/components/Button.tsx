5b7b64f1e482ab2dcbfe1f7c7e92ad78cc8980db
santiago-federici <santiagofederici88@gmail.com>
2024-04-04 19:44:26 -0300

    adjusted buttons styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,8 +2,8 @@ export function Button ({ as, text, extraClassName }: { as: string, text: string
 
   const defeaultButtonStyle = 'p-btn-padding rounded text-white font-bold'
   
-  const primaryButtonStyle = 'bg-primary font-medium'
-  const ghostButtonStyle = 'bg-transparent border-secondary font-bold border border-[2px]'
+  const primaryButtonStyle = 'bg-primary font-medium text-inverted'
+  const ghostButtonStyle = 'bg-transparent border-secondary font-bold border border-[2px] text-inverted'
   
   return (
     <button className={`${defeaultButtonStyle} ${as === 'primary' ? primaryButtonStyle : ghostButtonStyle} ${extraClassName}`}>{text}</button>
