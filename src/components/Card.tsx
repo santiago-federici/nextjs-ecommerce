a7aedb93bf6c5edb3043effb0a1cac911159faa4
santiago-federici <santiagofederici88@gmail.com>
2024-04-04 09:29:17 -0300

    created Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export function Card ({ prodName, price }: { prodName: string, price: string }) {
+  return (
+    <article className="grid gap-8 place-content-center">
+      <Image
+        src="/sunglass-1.png"
+        alt="sunglasses"
+        width={230}
+        height={114}
+        quality={100}
+        className="justify-self-center"
+        />
+
+      <div className="px-2 grid gap-2">
+        <h4>{prodName}</h4>
+        <p className="font-medium text-2xl">${price} <span className="text-green-500 text-xs align-text-top">15% OFF</span> </p>
+      </div>
+    </article>
+  )
+}
