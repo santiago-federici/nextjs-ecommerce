997a3b9353d3a0b88792450948fd9ec7092beead
santiago-federici <santiagofederici88@gmail.com>
2024-04-04 10:36:53 -0300

    increased the border stroke for the ghost buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ export function Button ({ as, text, extraClassName }: { as: string, text: string
   const defeaultButtonStyle = 'p-[--btn-padding] rounded text-white font-bold'
   
   const primaryButtonStyle = 'bg-black font-medium'
-  const ghostButtonStyle = 'bg-transparent border border-solid border-[--clr-secondary] font-bold'
+  const ghostButtonStyle = 'bg-transparent border border-[2px] border-[--clr-secondary] font-bold'
   
   return (
     <button className={`${defeaultButtonStyle} ${as === 'primary' ? primaryButtonStyle : ghostButtonStyle} ${extraClassName}`}>{text}</button>
