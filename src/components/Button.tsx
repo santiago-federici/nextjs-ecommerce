e090801f49aef31eb5abd1aa0b4f7b8a8ee75144
santiago-federici <santiagofederici88@gmail.com>
2024-04-05 16:35:22 -0300

    added hover to the buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
 export function Button ({ as, text, extraClassName }: { as: string, text: string, extraClassName?: string }) {
 
-  const defeaultButtonStyle = 'px-4 py-1 max-sm:text-sm md:p-btn-padding rounded font-bold'
+  const defeaultButtonStyle = 'px-4 py-1 max-sm:text-sm md:p-btn-padding rounded font-bold transition duration-200'
   
-  const primaryButtonStyle = 'text-inverted bg-primary font-medium'
-  const ghostButtonStyle = 'text-inverted bg-transparent border-secondary border border-[2px]'
+  const primaryButtonStyle = 'text-inverted bg-primary font-medium hover:opacity-80'
+  const ghostButtonStyle = 'text-inverted bg-transparent border-secondary border border-[2px] hover:bg-primary hover:border-primary hover:text-inverted'
   const buttonStyle = as === 'primary' ? primaryButtonStyle : ghostButtonStyle
   
   return (
