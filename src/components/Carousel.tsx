92a50eb06d3450c368df15e37d3b47088f0763ab
santiago-federici <santiagofederici88@gmail.com>
2024-04-05 18:11:00 -0300

    added a mask-image to the carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,8 +1,10 @@
 import { Card } from "./Card";
 
+import '@styles/Carousel.css'
+
 export function Carousel () {
   return (
-    <div className="carousel rounded-box w-full gap-4 md:gap-8">
+    <div className="carousel rounded-box w-full gap-4 md:gap-8 carousel-mask">
       <div className="carousel-item">
         <Card prodName="Gafas Aviador Cuero 2" price="5000" />
       </div> 
