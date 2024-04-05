7acac94016a9f6a046be51542ad9218f3b4d44ad
santiago-federici <santiagofederici88@gmail.com>
2024-04-05 09:29:33 -0300

    created a SectionTitle.tsx component for the home page titles

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -1,11 +1,11 @@
 import { Button } from "./Button";
 import { Card } from "./Card";
+import { SectionTitle } from "./SectionTitle";
 
 export function Trending () {
   return(
     <article className="grid gap-20 place-items-center mt-32 md:mt-44 ">
-      <h2 className="text-3xl md:text-4xl underline underline-offset-4 uppercase">Trending</h2>
-
+      <SectionTitle title="Trending" />
 
       <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8 ">
         <Card prodName="Gafas Aviador Cuero 2" price="5000" />
