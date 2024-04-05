bd064aba5b9e1dbd1da11672a5f20fbbf4a846b6
santiago-federici <santiagofederici88@gmail.com>
2024-04-05 17:50:14 -0300

    adjusted Carousel and Card sizings for small screens

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,12 +9,12 @@ export function Card ({ prodName, price }: { prodName: string, price: string }) 
         width={230}
         height={114}
         quality={100}
-        className="justify-self-center"
-        />
+        className="justify-self-center w-[150px] md:w-[230px]"
+      />
 
       <div className="px-2 grid gap-2">
-        <p className="text-xl">{prodName}</p>
-        <p className="font-medium text-2xl">${price} <span className="text-green-500 text-xs align-text-top">15% OFF</span> </p>
+        <p className="text-md md:text-xl">{prodName}</p>
+        <p className="font-medium text-xl md:text-2xl">${price} <span className="text-green-500 text-xs align-text-top">15% OFF</span> </p>
       </div>
     </article>
   )
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,7 +2,7 @@ import { Card } from "./Card";
 
 export function Carousel () {
   return (
-    <div className="carousel rounded-box w-full gap-8">
+    <div className="carousel rounded-box w-full gap-4 md:gap-8">
       <div className="carousel-item">
         <Card prodName="Gafas Aviador Cuero 2" price="5000" />
       </div> 
