5e42a06cb45c996ab42f77e2f8422635b65da6df
santiago-federici <santiagofederici88@gmail.com>
2024-04-05 10:48:21 -0300

    added responsivness to the text in Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export function Button ({ as, text, extraClassName }: { as: string, text: string, extraClassName?: string }) {
 
-  const defeaultButtonStyle = 'p-btn-padding rounded font-bold'
+  const defeaultButtonStyle = 'px-4 py-1 max-sm:text-sm md:p-btn-padding rounded font-bold'
   
   const primaryButtonStyle = 'text-inverted bg-primary font-medium'
   const ghostButtonStyle = 'text-inverted bg-transparent border-secondary border border-[2px]'
