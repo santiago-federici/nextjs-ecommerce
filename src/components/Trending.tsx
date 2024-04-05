2c5a4efb6cd2f332427628467a920f33627285e9
santiago-federici <santiagofederici88@gmail.com>
2024-04-05 10:50:53 -0300

    adjusted spaces in Trending.tsx

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -4,10 +4,10 @@ import { SectionTitle } from "./SectionTitle";
 
 export function Trending () {
   return(
-    <article className="grid gap-20 place-items-center mt-32 md:mt-44 ">
+    <article className="grid gap-14 place-items-center mt-20 md:mt-28 ">
       <SectionTitle title="Trending" />
 
-      <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8 ">
+      <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8 md:gap-12 ">
         <Card prodName="Gafas Aviador Cuero 2" price="5000" />
         <Card prodName="Gafas Aviador Cuero 2" price="5000" />
         <Card prodName="Gafas Aviador Cuero 2" price="5000" />
