ea44ffe61552b5e2cc232a56423cd747bfd0d9a9
santiago-federici <santiagofederici88@gmail.com>
2024-04-05 10:48:53 -0300

    re-styled the hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,15 +1,14 @@
 import { Button } from "@components/Button";
-import Image from "next/image";
+import '../styles/HeroSection.css'
 
 export function HeroSection () {
   return (
-    <div>
-      <section className="mt-14 md:mt-40 grid justify-items-center md:justify-items-start w-full">
-        <h1 className="text-white font-bold text-balance text-center lg:text-left lg:w-[500px]">
-          Mejora tu estilo con nuestros productos
-        </h1>
-        <Button as="ghost" text="Ver productos" extraClassName="mt-10" />
-      </section>
-    </div>
+    <section className="hero-bg mt-6 md:mt-14 w-full h-[200px] md:h-[400px] lg:h-[600px] flex flex-col justify-start items-start p-4 md:p-12 rounded-3xl special-shadow border border-slate-400">
+      <h1 className="text-white text-2xl md:text-5xl lg:text-6xl font-bold text-left w-[210px] md:w-[320px] lg:w-[450px]">
+        Mejora tu estilo con nuestros productos
+      </h1>
+      <Button as="ghost" text="Ver productos" extraClassName="mt-4 md:mt-10" />
+    </section>
   )
 }
+
