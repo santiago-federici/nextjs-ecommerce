56a47b3a8d052fe56c6126ee809833b5903cc3b6
santiago-federici <santiagofederici88@gmail.com>
2024-04-08 07:46:41 -0300

    created HomeSection, to reuse the styles

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -1,15 +1,16 @@
 import { Button } from "./Button";
 import { Carousel } from "./Carousel";
+import { HomeSection } from "./HomeSection";
 import { SectionTitle } from "./SectionTitle";
 
 export function Trending () {
   return(
-    <article className="grid gap-14 place-items-center mt-20 md:mt-28 ">
+    <HomeSection>
       <SectionTitle title="Trending" />
 
       <Carousel />
 
       <Button as="primary" text="Ir a productos" />
-    </article>
+    </HomeSection>
   )
 }
