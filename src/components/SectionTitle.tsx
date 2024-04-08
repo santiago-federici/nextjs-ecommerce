858328f192b20e1844c71156260c3b75165c0bdd
santiago-federici <santiagofederici88@gmail.com>
2024-04-08 07:47:19 -0300

    added z-index to the SectionTitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,5 +1,5 @@
 export function SectionTitle ({ title }: { title: string }) {
   return (
-    <h2 className="text-3xl md:text-4xl underline underline-offset-4 uppercase">{title}</h2>
+    <h2 className="text-3xl md:text-4xl underline underline-offset-4 uppercase z-10">{title}</h2>
   )
 }
