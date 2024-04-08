5b884b0656169a9381aba833b12d66d10edf9a86
santiago-federici <santiagofederici88@gmail.com>
2024-04-08 10:37:51 -0300

    removed border-radius to the carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,7 +4,7 @@ import '@styles/Carousel.css'
 
 export function Carousel () {
   return (
-    <div className="carousel rounded-box w-full gap-4 md:gap-8 carousel-mask">
+    <div className="carousel w-full gap-4 md:gap-8 carousel-mask">
       <div className="carousel-item">
         <Card prodName="Gafas Aviador Cuero 2" price="5000" />
       </div> 
