b6a1ca1d53be17bae6ffc836da2abe064ca522ec
santiago-federici <santiagofederici88@gmail.com>
2024-04-08 09:18:20 -0300

    added social media logos

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -162,3 +162,61 @@ export function Contact () {
     </svg>
   )
 }
+
+export function XLogo () {
+  return (
+    <svg  
+      width="35"  
+      height="35"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="currentColor"  
+      stroke-width="2"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+      <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+    </svg>
+  )
+}
+
+export function InstagramLogo () {
+  return (
+    <svg  
+      width="35"  
+      height="35"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="currentColor"  
+      stroke-width="2"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      <path d="M16.5 7.5l0 .01" />
+    </svg>
+  )
+}
+
+export function YoutubeLogo () {
+  return (
+    <svg  
+      width="35"  
+      height="35"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="currentColor"  
+      stroke-width="2"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M2 8a4 4 0 0 1 4 -4h12a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-8z" />
+      <path d="M10 9l5 3l-5 3z" />
+    </svg>
+  )
+}
