95a4f889770919d7794aa5304fdd5e38903b2736
santiago-federici <santiagofederici88@gmail.com>
2024-04-08 07:46:58 -0300

    added z-index to the Buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export function Button ({ as, text, extraClassName }: { as: string, text: string, extraClassName?: string }) {
 
-  const defeaultButtonStyle = 'px-4 py-1 max-sm:text-sm md:p-btn-padding rounded font-bold transition duration-200'
+  const defeaultButtonStyle = 'px-4 py-1 max-sm:text-sm md:p-btn-padding rounded font-bold transition duration-200 z-10'
   
   const primaryButtonStyle = 'text-inverted bg-primary font-medium hover:opacity-80'
   const ghostButtonStyle = 'text-inverted bg-transparent border-secondary border border-[2px] hover:bg-primary hover:border-primary hover:text-inverted'
