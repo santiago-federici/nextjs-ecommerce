38a1737397f60014a20f6ce2ad39f35bf8882ad0
santiago-federici <santiagofederici88@gmail.com>
2024-04-09 10:34:55 -0300

    uninstalled daysiui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,29 +2,8 @@ import { InstagramLogo, XLogo, YoutubeLogo } from "./Icons";
 
 export function Footer () {
   return (
-    <footer className="footer footer-center p-16 md:p-28 bg-base-200 text-slate-200 rounded md:text-lg mt-32 bg-footersurface">
-      <nav className="grid grid-flow-col gap-4">
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav> 
-      <nav>
-        <div className="grid grid-flow-col gap-4">
-          <a href="#" className="hover:scale-110 transition duration-200">
-            <XLogo />
-          </a>
-          <a href="#" className="hover:scale-110 transition duration-200">
-            <InstagramLogo />
-          </a>
-          <a href="#" className="hover:scale-110 transition duration-200">
-            <YoutubeLogo />
-          </a>
-        </div>
-      </nav> 
-      <aside>
-        <p>Copyright © 2024 - All right reserved by Ecom Industries Ltd</p>
-      </aside>
+    <footer>
+      Footer
     </footer>
   )
 }
--- src/components/HomePage/Carousel.tsx
+++ src/components/HomePage/Carousel.tsx
@@ -1,31 +1,9 @@
-import { Card } from "../Card";
-
 import '@styles/Carousel.css'
 
 export function Carousel () {
   return (
-    <div className="carousel w-full gap-4 md:gap-8 carousel-mask">
-      <div className="carousel-item">
-        <Card prodName="Gafas Aviador Cuero 2" price="5000" />
-      </div> 
-      <div className="carousel-item">
-        <Card prodName="Gafas Aviador Cuero 2" price="5000" />
-      </div> 
-      <div className="carousel-item">
-        <Card prodName="Gafas Aviador Cuero 2" price="5000" />
-      </div> 
-      <div className="carousel-item">
-        <Card prodName="Gafas Aviador Cuero 2" price="5000" />
-      </div> 
-      <div className="carousel-item">
-        <Card prodName="Gafas Aviador Cuero 2" price="5000" />
-      </div> 
-      <div className="carousel-item">
-        <Card prodName="Gafas Aviador Cuero 2" price="5000" />
-      </div> 
-      <div className="carousel-item">
-        <Card prodName="Gafas Aviador Cuero 2" price="5000" />
-      </div>
+    <div>
+      Carousel
     </div>
   )
 }
