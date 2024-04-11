d661d8da7328083d743038132c7c5ab5679217b7
santiago-federici <santiagofederici88@gmail.com>
2024-04-11 10:22:38 -0300

    fixed ghost button hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ export function Button ({ as, text, extraClassName }: { as: string, text: string
   const defeaultButtonStyle = 'text-inverted font-medium uppercase text-sm md:text-md p-btn-padding rounded-sm transition duration-200'
   
   const primaryButtonStyle = 'bg-secondary border border-secondary hover:opacity-70'
-  const ghostButtonStyle = 'bg-transparent border border-primary relative nice-hover'
+  const ghostButtonStyle = 'bg-transparent border border-primary relative z-10 ghost-btn-hover'
   const buttonStyle = as === 'primary' ? primaryButtonStyle : ghostButtonStyle
   
   return (
--- src/components/HomePage/HeroSection.tsx
+++ src/components/HomePage/HeroSection.tsx
@@ -4,13 +4,21 @@ import { Button } from '@components/Button'
 
 import '@/styles/HeroSection.css'
 
+const heroSectionInfo = {
+  title: 'Dress to Impress. Enter Our Fashion Wonderland',
+  btnText: 'Shop now'
+}
+
 export function HeroSection () {
   return (
     <section className='hero-bg'>
       <div className='wrapper pt-[220px] lg:pt-[250px] pb-[200px] grid place-items-center lg:place-items-start gap-8'>
-        <h1 className='text-white text-center lg:text-left lg:max-w-xl'>Dress to Impress. Enter Our Fashion Wonderland</h1>
+        <h1 className='text-white text-center lg:text-left lg:max-w-xl'>{heroSectionInfo.title}</h1>
 
-        <Button as='ghost' text='Go to products' />
+        <Button 
+          as='ghost' 
+          text={heroSectionInfo.btnText}
+        />
       </div>
     </section>
   )
