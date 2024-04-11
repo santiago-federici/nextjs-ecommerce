6c16c0e6f3763ff17882cd01a44017441446ca56
santiago-federici <santiagofederici88@gmail.com>
2024-04-11 14:48:25 -0300

    created a reusable component for the home page sections

--- src/components/HomePage/BlackSection.tsx
+++ src/components/HomePage/BlackSection.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { HomeSection } from "./HomeSection"
 
 const winterCollectionProds = [
   {
@@ -20,33 +21,31 @@ const winterCollectionProds = [
 
 export function BlackSection () {
   return(
-    <section className="bg-secondary w-full h-fit">
-      <div className='wrapper grid lg:flex gap-10 lg:gap-6 justify-between py-24 lg:py-32'>
-        <div className="flow lg:max-w-sm xl:max-w-xl">
-          <h3 className="text-white">CHECK OUT OUR WINTER COLLECTION</h3>
-          <p className="text-gray-300">Winter Collections — New Modern Design</p>
-          <p className="text-gray-300">We’ve gathered the latest models for you to consider adding to your winter lineup, check and see which ones can complete your wardrobe this season.</p>
-        </div>
-
-        <ul className="grid lg:flex gap-4 w-full">
-          {
-            winterCollectionProds.map((prod, index) => (
-              <li key={index} className="relative overflow-hidden group">
-                <Image
-                  src={prod.image}
-                  alt={prod.alt}
-                  width={500}
-                  height={0}
-                  className="w-full h-[300px] mg:h-[350px] lg:h-[400px] object-cover opacity-75 cursor-pointer group-hover:opacity-85 group-hover:scale-110 transition duration-500"
-                />
-
-
-                <p className="absolute text-white font-bold bottom-0 right-2 cursor-pointer custom-underline">Discover</p>
-              </li>
-            ))
-          }
-        </ul>
+    <HomeSection bgColor="secondary" extraClassname="justify-between">
+      <div className="flow lg:max-w-sm xl:max-w-xl">
+        <h3 className="text-white">CHECK OUT OUR WINTER COLLECTION</h3>
+        <p className="text-gray-300">Winter Collections — New Modern Design</p>
+        <p className="text-gray-300">We’ve gathered the latest models for you to consider adding to your winter lineup, check and see which ones can complete your wardrobe this season.</p>
       </div>
-    </section>
+
+      <ul className="grid lg:flex gap-4 w-full">
+        {
+          winterCollectionProds.map((prod, index) => (
+            <li key={index} className="relative overflow-hidden group">
+              <Image
+                src={prod.image}
+                alt={prod.alt}
+                width={500}
+                height={0}
+                className="w-full h-[300px] mg:h-[350px] lg:h-[400px] object-cover opacity-75 cursor-pointer group-hover:opacity-85 group-hover:scale-110 transition duration-500"
+              />
+
+
+              <p className="absolute text-white font-bold bottom-0 right-2 cursor-pointer custom-underline">Discover</p>
+            </li>
+          ))
+        }
+      </ul>
+    </HomeSection>
   )
 }
