fca9ef39a90171d1c1bb0539a8e41f5a7c97576b
santiago-federici <santiagofederici88@gmail.com>
2024-04-11 13:24:39 -0300

    added styles to the BlackSection for bigger screens

--- src/components/HomePage/BlackSection.tsx
+++ src/components/HomePage/BlackSection.tsx
@@ -21,14 +21,14 @@ const winterCollectionProds = [
 export function BlackSection () {
   return(
     <section className="bg-secondary w-full h-fit">
-      <div className='wrapper grid lg:flex gap-16 justify-between py-24 lg:py-32'>
-        <div className="flow">
+      <div className='wrapper grid lg:flex gap-16 lg:gap-6 justify-between py-24 lg:py-32'>
+        <div className="flow lg:max-w-sm xl:max-w-xl">
           <h3 className="text-white">CHECK OUT OUR WINTER COLLECTION</h3>
           <p className="text-gray-300">Winter Collections — New Modern Design</p>
           <p className="text-gray-300">We’ve gathered the latest models for you to consider adding to your winter lineup, check and see which ones can complete your wardrobe this season.</p>
         </div>
 
-        <ul className="grid lg:flex gap-6 w-full">
+        <ul className="grid lg:flex gap-4 w-full">
           {
             winterCollectionProds.map((prod, index) => (
               <li key={index} className="relative">
@@ -37,10 +37,10 @@ export function BlackSection () {
                   alt={prod.alt}
                   width={500}
                   height={0}
-                  className="w-full h-[200px] object-cover opacity-75"
+                  className="w-full h-[300px] mg:h-[350px] lg:h-[400px] object-cover opacity-75"
                 />
 
-                <p className="absolute text-black font-bold bottom-0 right-2 cursor-pointer custom-underline">Discover</p>
+                <p className="absolute text-white font-bold bottom-0 right-2 cursor-pointer custom-underline">Discover</p>
               </li>
             ))
           }
