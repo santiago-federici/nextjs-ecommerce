f9af345e2fbb3827926c35efabaff02e760b3fe1
santiago-federici <santiagofederici88@gmail.com>
2024-04-13 17:20:14 -0300

    footer update: created and styled the footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,51 @@
 import { InstagramLogo, XLogo, YoutubeLogo } from "./Icons";
 
+const navLinks = [
+  {
+    title: 'Home'
+  },
+  {
+    title: 'Products'
+  },
+  {
+    title: 'About'
+  }
+]
+
+const socialLinks = [
+  {
+    icon: <InstagramLogo />
+  },
+  {
+    icon: <YoutubeLogo />
+  },
+  {
+    icon: <XLogo />
+  }
+]
+
 export function Footer () {
   return (
-    <footer>
+    <footer className="bg-footer-surface py-20">
+      <div className="wrapper text-gray-200 grid gap-8 place-items-center text-lg lg:text-xl">
+        <ul className="flex gap-4">
+          {
+            navLinks.map((link, index) => (
+              <li key={index} className="hover:text-accent cursor-pointer transition duration-200">{link.title}</li>
+            ))
+          }
+        </ul>
+
+        <ul className="flex gap-4">
+          {
+            socialLinks.map((link, index) => (
+              <li key={index} className="hover:bg-slate-700 p-1 rounded-xl cursor-pointer transition duration-200">{link.icon}</li>
+            ))
+          }
+        </ul>
+
+        <p className="text-sm text-center hover:text-accent cursor-pointer transition duration-200">Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+      </div>
     </footer>
   )
 }
