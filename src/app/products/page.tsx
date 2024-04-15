995259599b16af38960747067bddf386bca24987
santiago-federici <santiagofederici88@gmail.com>
2024-04-15 08:03:33 -0300

    productsPage: created products page with filters sectin (no logic yet)

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,15 @@
-import { Button } from "@components/CustomButtons";
-import { Filter, Search } from "@components/Icons";
-import Image from "next/image";
+import Image from "next/image"
+
+import { Filters } from "@components/ProductsPage/Filters"
 
 export default function ProductsPage () {
+
   return(
     <div className='wrapper pt-[120px] pb-[200px]'>
 
+      {/* TODO: create breadcrumbs component with all the logic */}
       <p className="text-gray-500 text-sm mb-10">Homepage / Products</p>
 
-      {/* ProductsHeader */}
       <section className="grid lg:flex justify-between w-full">
         <div className="flex flex-col justify-between">
           <h2 className="text-6xl">Shirts</h2>
@@ -18,13 +19,9 @@ export default function ProductsPage () {
         <Image src={'/prodsHeader.png'} width={900} height={150} alt={'prodsHeader'} className="w-full lg:w-2/3" />
       </section>
 
-      {/* Filters */}
-      <section className="mt-10 flex justify-between">
-        <Button as="ghost" text="Filters" icon={<Filter />} extraClassName="flex gap-2 border-secondary text-secondary" />
-
-        <Button as="ghost" text="Search" icon={<Search />} extraClassName="flex gap-2 border-secondary text-secondary" />
-      </section>
+      <Filters />
+      
 
     </div>
   )
-}
+} 
