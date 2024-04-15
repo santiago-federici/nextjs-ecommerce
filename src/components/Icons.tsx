8a00f7c500179c5f9f0d446aa7fb34d94216721a
santiago-federici <santiagofederici88@gmail.com>
2024-04-15 11:15:27 -0300

    icons update: added chevron right

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -258,3 +258,21 @@ export function ArrowUp () {
     </svg>
   )
 }
+
+export function ArrowRight () {
+  return (
+    <svg  
+      width="20"  
+      height="20"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="currentColor"  
+      stroke-width="1"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M9 6l6 6l-6 6" />
+    </svg>
+  )
+}
