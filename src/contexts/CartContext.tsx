1c33a32baf830949fbe664c23c4810e69e54b191
santiago-federici <santiagofederici88@gmail.com>
2024-04-16 11:38:24 -0300

    created a CartContext with all the necessary methods

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,79 @@
+'use client'
+
+import { createContext, useState } from "react";
+
+type CartContext = {
+  increaseQuantity: (id: number) => void
+  decreaseQuantity: (id: number) => void
+  removeProd: (id: number) => void
+  clearCart: () => void
+
+  cartQuantity: number
+  cart: CartItem[]
+}
+
+type CartItem = {
+  id: number
+  quantity: number
+}
+
+export const CartContext = createContext({} as CartContext)
+
+export function CartProvider ({ children }: { children: React.ReactNode }) {
+  const [cart, setCart] = useState<CartItem[]>([])
+
+  const increaseQuantity = (id: number) => {
+    const indexOfProduct = cart.findIndex(item => item.id === id)
+
+    if (indexOfProduct >= 0) {
+      const newCart = structuredClone(cart)
+      newCart[indexOfProduct].quantity += 1
+      return setCart(newCart)
+    }
+
+    setCart(prevState => [
+      ...prevState,
+      {
+        id,
+        quantity: 1
+      }])
+  }
+  
+  const decreaseQuantity = (id: number) => {
+    const indexOfProduct = cart.findIndex(item => item.id === id)
+    if (indexOfProduct >= 0) {
+      const newCart = structuredClone(cart)
+      if (newCart[indexOfProduct].quantity > 1) {
+        newCart[indexOfProduct].quantity -= 1
+        return setCart(newCart)
+      }
+      if (newCart[indexOfProduct].quantity === 1) {
+        removeProd(id)
+      }
+    }
+  }
+
+  const removeProd = (id: number) => {
+    const newCart = cart.filter(item => item.id !== id)
+    setCart(newCart)
+  }
+
+  const clearCart = () => {
+    setCart([])
+  }
+
+  const cartQuantity = cart.length
+  
+  return(
+    <CartContext.Provider value={{
+      increaseQuantity,
+      removeProd,
+      decreaseQuantity,
+      clearCart,
+      cart,
+      cartQuantity
+    }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
