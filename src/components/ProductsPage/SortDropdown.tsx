899581a2af36922632ea6564d37f512b4a97cbd9
santiago-federici <santiagofederici88@gmail.com>
2024-04-16 12:08:16 -0300

    sortDropdown update: incresed z-index

--- src/components/ProductsPage/SortDropdown.tsx
+++ src/components/ProductsPage/SortDropdown.tsx
@@ -107,7 +107,7 @@ export function SortDropdown () {
       <motion.ul
         variants={ulVariants}
         style={{ pointerEvents: isOpen ? "auto" : "none" }}
-        className="absolute bg-zinc-300 w-fit mt-2 text-nowrap grid gap-2 z-20"
+        className="absolute bg-zinc-300 w-fit mt-2 text-nowrap grid gap-2 z-50"
       >
         {
           sortOptions.map((option, index) => (
