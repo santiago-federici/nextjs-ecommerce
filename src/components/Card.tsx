c05eb7b4cf2b7e3d52b46959c16ff3d31a0faa3e
santiago-federici <santiagofederici88@gmail.com>
2024-04-16 11:41:08 -0300

    card update: it now only requires id as parameter and does a 'find' to look for the rest of the product

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,33 +1,41 @@
+'use client'
+
 import Image from "next/image";
 
-export function Card({ prodName, price, prevPrice, image, isOffer }: { prodName: string, price: number, prevPrice?: number, image: string, isOffer?: number }) {
+import prods from '@mocks/prods.json'
+
+export function Card({ id }: { id: number }) {
+
+  const prod = prods.find(prod => prod.id === id)
+  
   return (
+    prod &&
     <article className="grid bg-white rounded-sm shadow-md overflow-hidden">
       <div className="relative h-60">
         <Image
-          src={image}
-          alt={prodName}
+          src={prod.image}
+          alt={prod.prodName}
           layout="fill"
           objectFit="cover"
         />
 
         {
-          isOffer && 
-          <span className="bg-green-500 text-white text-sm font-semibold py-1 px-2 rounded-md absolute top-2 right-2">{isOffer}
+          prod.isOffer && 
+          <span className="bg-green-500 text-white text-sm font-semibold py-1 px-2 rounded-md absolute top-2 right-2">{prod.isOffer}
             % OFF
           </span>
         }
       </div>
 
       <div className="p-4">
-        <h3 className="text-lg">{prodName}</h3>
+        <h3 className="text-lg">{prod.prodName}</h3>
         <div className="grid items-center">
           {
-            prevPrice && <p className="text-sm text-gray-500 line-through mt-1">${prevPrice}</p>
+            prod.prevPrice && <p className="text-sm text-gray-500 line-through mt-1">${prod.prevPrice}</p>
           }
-          <p className="text-gray-900 text-2xl">${price}</p>
+          <p className="text-gray-900 text-2xl">${prod.price}</p>
         </div>
       </div>
     </article>
-  );
+  )
 }
