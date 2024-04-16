63389100c8813dac4efb0b7c526ea112e2975d85
santiago-federici <santiagofederici88@gmail.com>
2024-04-16 08:24:14 -0300

    carousel update: created a mask-image

--- src/components/CarouselContainer.tsx
+++ src/components/CarouselContainer.tsx
@@ -10,30 +10,30 @@ import { Card } from "./Card"
 
 export function CarouselContainer ({ prods }: { prods: any[] }) {
   return(
-      <Carousel
-        opts={{
-          align: "start",
-          loop: true,
-        }}
-        className="overflow-hidden w-full"
+    <Carousel
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      className="overflow-hidden w-full carousel-mask-image"
       >
-        <CarouselContent className="w-full gap-4">
-          {
-            prods.map((prod, index) => (
-              <CarouselItem key={index} className="w-full basis-full md:basis-1/2 lg:basis-1/3">
-                <Card
-                  prodName={prod.prodName}
-                  price={prod.price}
-                  prevPrice={prod.prevPrice}
-                  image={prod.image}
-                  isOffer={prod.isOffer}
-                />
-              </CarouselItem>
-            ))
-          }
-        </CarouselContent>
-        <CarouselPrevious className="ml-14 md:top-[45%]" />
-        <CarouselNext className="mr-14 md:top-[45%]" />
-      </Carousel>
+      <CarouselContent className="w-full gap-4">
+        {
+          prods.map((prod, index) => (
+            <CarouselItem key={index} className="w-full basis-full md:basis-1/2 lg:basis-1/3">
+              <Card
+                prodName={prod.prodName}
+                price={prod.price}
+                prevPrice={prod.prevPrice}
+                image={prod.image}
+                isOffer={prod.isOffer}
+              />
+            </CarouselItem>
+          ))
+        }
+      </CarouselContent>
+      <CarouselPrevious className="ml-24 md:top-[45%]" />
+      <CarouselNext className="mr-24 md:top-[45%]" />
+    </Carousel>
   )
 }
