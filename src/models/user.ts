65417c265964ec11f4f82f0795f446c5ad3e5b1c
santiago-federici <santiagofederici88@gmail.com>
2024-04-22 17:38:45 -0300

    user schema update: deleted extra space

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,26 +1,26 @@
-import { Schema, model, models } from 'mongoose'
+import { Schema, model, models } from "mongoose";
 
-const UserSchema = new Schema ({
+const UserSchema = new Schema({
   email: {
     type: String,
-    unique: [true, 'Email already exists'],
-    required: [true, 'Email is required'],
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Invalid Email']
+    unique: [true, "Email already exists"],
+    required: [true, "Email is required"],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Invalid Email"],
   },
   password: {
     type: String,
-    required: [true, 'Password is required'],
-    select: false
+    required: [true, "Password is required"],
+    select: false,
   },
   username: {
     type: String,
-    required: [true, 'Username is required'],
-    minLength: [3, 'Username must be at least 3 characters'],
-    maxLength: [20, 'Username must be less than 20 characters'],
-    match: [/^[a-zA-Z0-9]+$/, 'Invalid username']
-  }
-})
+    required: [true, "Username is required"],
+    minLength: [3, "Username must be at least 3 characters"],
+    maxLength: [20, "Username must be less than 20 characters"],
+    match: [/^[a-zA-Z0-9]+$/, "Invalid username"],
+  },
+});
 
-const User = models.User || model ('User', UserSchema)
+const User = models.User || model("User", UserSchema);
 
-export default User
+export default User;
