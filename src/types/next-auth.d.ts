ba637a2b27222ab9e65256be4e9456dd18e6f796
santiago-federici <santiagofederici88@gmail.com>
2024-05-07 19:38:47 -0300

    created a file to provide id to the session.user

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,11 @@
+// a method to get the user id in session.user.id
+
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
