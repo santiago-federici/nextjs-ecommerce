2ba97b5a7e68aaeefd0717f88110cf436bb4396c
santiago-federici <santiagofederici88@gmail.com>
2024-05-10 18:16:32 -0300

    isValidEmail method created inside utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// method to test if emails are valid
+export const isValidEmail = (email: any) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
