d73f4f4293bc628caa662cfcde6ca56e2a7ac19c
santiago-federici <santiagofederici88@gmail.com>
2024-05-14 11:10:08 -0300

    updated next.config to accept images from a custom domain. (that method is deprecated, fix it later)

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/a/**",
-      }
-    ]
+    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
   }
 };
 
