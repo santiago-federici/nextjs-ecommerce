5374d459d92f95e03fad8fe70425984b57bf769e
santiago-federici <santiagofederici88@gmail.com>
2024-05-15 10:51:34 -0300

    created formatPrice function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 // method to test if emails are valid
 export const isValidEmail = (email: any) =>
   /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+
+export const formatPrice = (price: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+};
