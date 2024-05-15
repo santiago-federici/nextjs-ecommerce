794d9023aa8bfaf5caeb4450d2c7d4c60b226a1c
santiago-federici <santiagofederici88@gmail.com>
2024-05-15 08:36:01 -0300

    sortDropdown update: updated the sort options

--- src/components/ProductsPage/SortDropdown.tsx
+++ src/components/ProductsPage/SortDropdown.tsx
@@ -1,4 +1,6 @@
-'use client'
+"use client";
+
+import { useState } from "react";
 
 import {
   DropdownMenu,
@@ -8,81 +10,54 @@ import {
   DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Sort } from "@components/Icons"
-import { useState } from "react"
+} from "@/components/ui/dropdown-menu";
 
+import { Sort } from "@components/Icons";
 
 const sortOptions = [
   {
-    name: "Price: Low to High",
-    value: "price-asc"
+    name: "Most relevant",
+    value: "most-relevant",
   },
   {
-    name: "Price: High to Low",
-    value: "price-desc"
+    name: "Most recent",
+    value: "most-recent",
   },
   {
-    name: "Name: A to Z",
-    value: "name-asc"
+    name: "Highest price",
+    value: "highest-price",
   },
   {
-    name: "Name: Z to A",
-    value: "name-desc"
+    name: "Lowest price",
+    value: "lowest-price",
   },
-  {
-    name: "Rating: High to Low",
-    value: "rating-desc"
-  },
-  {
-    name: "Rating: Low to High",
-    value: "rating-asc"
-  },
-  {
-    name: "Discount: High to Low",
-    value: "discount-desc"
-  },
-  {
-    name: "Discount: Low to High",
-    value: "discount-asc"
-  },
-  {
-    name: "Popularity",
-    value: "popularity"
-  },
-  {
-    name: "Latest",
-    value: "latest"
-  },
-]
+];
 
-export function SortDropdown ({ className }: { className: string }) {
-  const [value, setValue] = useState('latest')
-  
+export function SortDropdown({ className }: { className: string }) {
+  const [value, setValue] = useState("latest");
+
   return (
     <div className={className}>
       <DropdownMenu>
-        <DropdownMenuTrigger  className="font-medium uppercase text-sm md:text-md px-4 py-2 rounded-md relative transition duration-200 flex gap-2 bg-zinc-300 border border-zinc-900 hover:opacity-70">
+        <DropdownMenuTrigger className="font-medium uppercase text-sm md:text-md px-4 py-2 rounded-md relative transition duration-200 flex gap-2 bg-zinc-300 border border-zinc-900 hover:opacity-70">
           <Sort /> Sort
         </DropdownMenuTrigger>
-        <DropdownMenuContent className='ml-4 lg:ml-0'>
+        <DropdownMenuContent className="ml-4 lg:ml-0">
           <DropdownMenuLabel>Sort By</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuRadioGroup value={value}>
-            {
-              sortOptions.map((option, index) => (
-                <DropdownMenuRadioItem 
-                  key={index}
-                  value={option.value}
-                  onClick={() => setValue(option.value)}
-                >
-                  {option.name}
-                </DropdownMenuRadioItem>  
-              ))
-            }
+            {sortOptions.map((option, index) => (
+              <DropdownMenuRadioItem
+                key={index}
+                value={option.value}
+                onClick={() => setValue(option.value)}
+              >
+                {option.name}
+              </DropdownMenuRadioItem>
+            ))}
           </DropdownMenuRadioGroup>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
-  )
+  );
 }
