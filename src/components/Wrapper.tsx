7dc387dab8c91ba4141aadfc80172c6ae21b6066
santiago-federici <santiagofederici88@gmail.com>
2024-05-20 07:24:07 -0300

    added a custom bg to the wrapper (bg.ibelick)

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,7 +1,14 @@
-export function Wrapper ({ className, children }: { className?: string, children?: React.ReactNode }) {
+export function Wrapper({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) {
   return (
     <section className={`wrapper ${className}`}>
+      <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
       {children}
     </section>
-  )
+  );
 }
