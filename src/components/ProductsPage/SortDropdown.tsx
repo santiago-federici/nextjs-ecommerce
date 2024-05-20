f7a870bf3f94d3a408cbd96701929f57156972bc
santiago-federici <santiagofederici88@gmail.com>
2024-05-20 10:42:06 -0300

    sort dropdown update: ajusted some paddings

--- src/components/ProductsPage/SortDropdown.tsx
+++ src/components/ProductsPage/SortDropdown.tsx
@@ -44,8 +44,8 @@ export function SortDropdown({ className }: { className: string }) {
         <DropdownMenuTrigger className="flex items-center gap-2 bg-white text-sm font-medium uppercase pl-3 pr-4 py-2 border border-gray-200 rounded-md hover:bg-gray-100 transition duration-200">
           <Sort /> Sort
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="ml-4 lg:ml-0">
-          <DropdownMenuLabel>Sort By</DropdownMenuLabel>
+        <DropdownMenuContent className="ml-4 lg:ml-0 pr-4 pl-0">
+          <DropdownMenuLabel className="pl-6">Sort By</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuRadioGroup value={value}>
             {sortOptions.map((option, index) => (
