c8f9a254a5ba7dd0109087109ea1cfc3784aa9c3
santiago-federici <santiagofederici88@gmail.com>
2024-05-20 07:40:48 -0300

    changed wrapper background

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -7,7 +7,7 @@ export function Wrapper({
 }) {
   return (
     <section className={`wrapper ${className}`}>
-      <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
+      <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
       {children}
     </section>
   );
