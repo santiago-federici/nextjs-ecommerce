15779238c8659b9ef8a50fce50ecba2b64d3c8e4
santiago-federici <santiagofederici88@gmail.com>
2024-05-28 10:07:36 -0300

    restyled the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,59 +1,72 @@
 import Link from "next/link";
+import { LogoSVG } from "@components/Icons";
 import { InstagramLogo, XLogo, YoutubeLogo } from "./Icons";
+import { Separator } from "./ui/separator";
 
 const navLinks = [
   {
-    title: 'Home'
+    title: "Home",
   },
   {
-    title: 'Products'
+    title: "Products",
   },
   {
-    title: 'About'
-  }
-]
+    title: "About",
+  },
+];
 
 const socialLinks = [
   {
-    icon: <InstagramLogo />
+    icon: <InstagramLogo />,
   },
   {
-    icon: <YoutubeLogo />
+    icon: <YoutubeLogo />,
   },
   {
-    icon: <XLogo />
-  }
-]
+    icon: <XLogo />,
+  },
+];
 
-export function Footer () {
+export function Footer() {
   return (
-    <footer className="bg-footer-surface py-20">
-      <div className="wrapper text-gray-200 grid gap-8 place-items-center text-lg lg:text-xl">
-        <ul className="flex gap-4">
-          {
-            navLinks.map((link, index) => (
+    <footer className="bg-footer-surface py-4">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Link
+            href="/"
+            className="flex items-center mb-4 sm:mb-0 gap-2 rtl:space-x-reverse"
+          >
+            <span className="bg-white rounded-full p-1 cursor-pointer transition duration-200">
+              <LogoSVG width="30" height="30" />
+            </span>
+            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
+              Urban Vogue
+            </span>
+          </Link>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 gap-4">
+            {navLinks.map((link, index) => (
               <li key={index}>
                 <Link
-                  href={`/${link.title === 'Home' ? '' : link.title.toLowerCase()}`}
+                  href={`/${
+                    link.title === "Home" ? "" : link.title.toLowerCase()
+                  }`}
                   className="hover:text-custom-accent cursor-pointer transition duration-200"
                 >
                   {link.title}
                 </Link>
               </li>
-            ))
-          }
-        </ul>
-
-        <ul className="flex gap-4">
-          {
-            socialLinks.map((link, index) => (
-              <li key={index} className="hover:bg-slate-700 p-1 rounded-xl cursor-pointer transition duration-200">{link.icon}</li>
-            ))
-          }
-        </ul>
-
-        <p className="text-sm text-center hover:text-custom-accent cursor-pointer transition duration-200">Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+            ))}
+          </ul>
+        </div>
+        <Separator className="my-6 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center">
+          © 2023{" "}
+          <Link href="/" className="hover:underline">
+            Urban Vogue™
+          </Link>
+          . All Rights Reserved.
+        </span>
       </div>
     </footer>
-  )
+  );
 }
