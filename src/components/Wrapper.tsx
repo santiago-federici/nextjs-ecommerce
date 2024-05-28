b48d49f986f4a813147a110206e7b330c9c4284e
santiago-federici <santiagofederici88@gmail.com>
2024-05-28 09:50:46 -0300

    removed bg.ibelick in Wrapper component

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -5,10 +5,5 @@ export function Wrapper({
   className?: string;
   children?: React.ReactNode;
 }) {
-  return (
-    <section className={`wrapper ${className}`}>
-      <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
-      {children}
-    </section>
-  );
+  return <section className={`wrapper ${className}`}>{children}</section>;
 }
