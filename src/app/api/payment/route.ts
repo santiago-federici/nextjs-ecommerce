bf756149d1c7f228d0c0348de9133c2ac8795ac3
santiago-federici <santiagofederici88@gmail.com>
2024-06-13 13:04:02 -0300

    commented all code related to MercadoPago

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,31 +1,31 @@
-import MercadoPagoConfig, { Payment } from "mercadopago";
-import { NextRequest } from "next/server";
+// import MercadoPagoConfig, { Payment } from "mercadopago";
+// import { NextRequest } from "next/server";
 
-const client = new MercadoPagoConfig({
-  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN!,
-});
+// const client = new MercadoPagoConfig({
+//   accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN!,
+// });
 
-export async function POST(req: NextRequest) {
-  const body = await req
-    .json()
-    .then((data) => data as { data: { id: string } });
+// export async function POST(req: NextRequest) {
+//   const body = await req
+//     .json()
+//     .then((data) => data as { data: { id: string } });
 
-  // const secret = req.headers.get("x-signature-id");
+//   // const secret = req.headers.get("x-signature-id");
 
-  // This if statement is for production. It chekcs that the payment is valid. The secret is found in Webhooks. --> https://www.mercadopago.com.ar/developers/panel/app/2654287895346889/webhooks
-  // if (secret !== process.env.MERCADOPAGO_SECRET)
-  // return Response.json({ success: false });
+//   // This if statement is for production. It chekcs that the payment is valid. The secret is found in Webhooks. --> https://www.mercadopago.com.ar/developers/panel/app/2654287895346889/webhooks
+//   // if (secret !== process.env.MERCADOPAGO_SECRET)
+//   // return Response.json({ success: false });
 
-  const payment = await new Payment(client).get({ id: body.data.id });
+//   const payment = await new Payment(client).get({ id: body.data.id });
 
-  const receivedPayment = {
-    id: payment.id!,
-    amount: payment.transaction_amount!,
-    message: payment.description!,
-    createdAt: payment.date_created!,
-  };
+//   const receivedPayment = {
+//     id: payment.id!,
+//     amount: payment.transaction_amount!,
+//     message: payment.description!,
+//     createdAt: payment.date_created!,
+//   };
 
-  // await db.insert(payments).values(receivedPayment);
+//   // await db.insert(payments).values(receivedPayment);
 
-  return Response.json({ success: true });
-}
+//   return Response.json({ success: true });
+// }
--- src/app/checkout/_components/CheckoutLogic.tsx
+++ src/app/checkout/_components/CheckoutLogic.tsx
@@ -1,43 +1,43 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-import { MercadoPagoConfig, Preference } from "mercadopago";
+// import { MercadoPagoConfig, Preference } from "mercadopago";
 
 import { Button } from "@components/ui/button";
 import { Label } from "@components/ui/label";
 import { Input } from "@components/ui/input";
 
-const client = new MercadoPagoConfig({
-  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN!,
-});
+// const client = new MercadoPagoConfig({
+//   accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN!,
+// });
 
 export default function CheckoutLogic() {
-  async function pay() {
-    "use server";
+  // async function pay() {
+  //   "use server";
 
-    const preference = await new Preference(client).create({
-      body: {
-        items: [
-          {
-            id: "1",
-            title: "testing prod",
-            quantity: 1,
-            unit_price: 1005,
-          },
-        ],
-        back_urls: {
-          failure: "http://localhost:3000/payment/failure",
-          success: "http://localhost:3000/payment/success",
-          pending: "http://localhost:3000/payment/pending",
-        },
-      },
-    });
+  //   const preference = await new Preference(client).create({
+  //     body: {
+  //       items: [
+  //         {
+  //           id: "1",
+  //           title: "testing prod",
+  //           quantity: 1,
+  //           unit_price: 1005,
+  //         },
+  //       ],
+  //       back_urls: {
+  //         failure: "http://localhost:3000/payment/failure",
+  //         success: "http://localhost:3000/payment/success",
+  //         pending: "http://localhost:3000/payment/pending",
+  //       },
+  //     },
+  //   });
 
-    redirect(preference.sandbox_init_point!);
-  }
+  //   redirect(preference.sandbox_init_point!);
+  // }
 
   return (
     <section className="bg-gray-50 w-full h-full px-4 py-12 flex flex-col items-center">
-      <form action={pay} className="w-full max-w-72 flex flex-col gap-4">
+      <form /*action={pay}*/ className="w-full max-w-72 flex flex-col gap-4">
         <div>
           <Label>
             Card information<span className="text-red-500">*</span>
