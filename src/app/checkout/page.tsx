d534c9470296148bf96dc82465170140a6e1120b
santiago-federici <santiagofederici88@gmail.com>
2024-06-13 10:10:00 -0300

    added usefull comment

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,6 +2,7 @@ import { Wrapper } from "@components/Wrapper";
 import CheckoutProds from "./_components/CheckoutProds";
 import CheckoutLogic from "./_components/CheckoutLogic";
 
+// If you want to implement Wix's checkout page and management, follow along with this tutorial: https://www.youtube.com/watch?v=I0BOUiFe9WY (4:48:00)
 export default async function CheckoutPage() {
   return (
     <Wrapper className="flex border rounded-md overflow-hidden h-fit mt-8">
